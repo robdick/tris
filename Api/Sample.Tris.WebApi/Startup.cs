ff872324815b2e42a7a6957a8e7f1a55680a766e
Rob Dick <r.dick@waracle.com>
2019-11-07 22:28:27 +0000

    Add swagger support

--- Api/Sample.Tris.WebApi/Startup.cs
+++ Api/Sample.Tris.WebApi/Startup.cs
@@ -2,13 +2,14 @@
 
 namespace Sample.Tris.WebApi
 {
+    using System;
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Cors.Infrastructure;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.OpenApi.Models;
     using Sample.Tris.WebApi.Configuration;
 
     public class Startup
@@ -35,8 +36,21 @@ namespace Sample.Tris.WebApi
                     {
                         return new BadRequestObjectResult(context.ModelState);
                     };
-                })
-                ;
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "Tris Query API",
+                    Version = "v1",
+                    Description = "Triangle Grid Query API",
+                    Contact = new OpenApiContact() {
+                        Name = "Rob Dick",
+                        Email = "robdick@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/robdick/")
+                    }
+                });
+            });
 
             var mapperInstance = AutomapperConfiguration.CreateMapper();
             services.AddSingleton(mapperInstance);
@@ -48,6 +62,12 @@ namespace Sample.Tris.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app
+                .UseSwagger()
+                .UseSwaggerUI(setupAction => {
+                    setupAction.SwaggerEndpoint("/swagger/v1/swagger.json", "Triangle Grid Query API");
+                });
 
             app
                 .UseStaticFiles()
